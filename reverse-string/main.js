d54f08a297d63a559c7f30d4fde7f4bb7e01aa0b
kaushnian <kefbox@gmail.com>
2018-07-25 20:41:35 -0700

    remove console.log

--- reverse-string/main.js
+++ reverse-string/main.js
@@ -6,11 +6,8 @@ let result = [];
 const charsLength = chars.length;
 
 for (let i = 0, j = charsLength; i <= Math.floor(charsLength / 2); i++, j = charsLength - i) {
-  console.log(i, j);
-  
   result[j] = chars[i];
   result[i] = chars[j];
-
 }
 
 console.log(result.join(''));
