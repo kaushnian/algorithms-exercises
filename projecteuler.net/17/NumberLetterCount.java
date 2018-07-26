7a145646af724b2e24704ea05ba014ea90b8e8a6
kaushnian <kefbox@gmail.com>
2018-07-25 17:10:38 -0700

    Add projecteuler.net

---
+++ projecteuler.net/17/NumberLetterCount.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * NumberLetterCount
+ */
+public class NumberLetterCount {
+
+  public static void main(String[] args) {
+    
+    Map<Integer, String> letters = new Map<Integer, String>();
+    
+    letters.put(1, "one");
+    letters.put(2, "one");
+    letters.put(3, "one");
+    letters.put(4, "one");
+    letters.put(5, "one");
+    letters.put(6, "one");
+    letters.put(7, "one");
+    letters.put(8, "one");
+    letters.put(9, "one");
+    letters.put(10, "one");
+    letters.put(11, "one");
+    letters.put(12, "one");
+    letters.put(13, "one");
+    letters.put(1, "one");
+    letters.put(1, "one");
+    letters.put(1, "one");
+    letters.put(1, "one");
+    letters.put(1, "one");
+    letters.put(1, "one");
+    letters.put(20, "one");
+
+    int sum = 0;
+    
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    for(int i = 1; i <= 1000; i++) {
+      
+      System.out.println("");
+    }
+
+  }
+}
