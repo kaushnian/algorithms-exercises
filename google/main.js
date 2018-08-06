871d422b1f8ad54e39c77c3dcd4e535bd3011ad4
kaushnian <kefbox@gmail.com>
2018-08-05 21:14:27 -0700

    Optimize the algorithm

--- google/main.js
+++ google/main.js
@@ -13,10 +13,15 @@ const m = matrix.length;
 let result = [];
 let current;
 
-for (let i = 0; i < n; i++) {
-  result.push([]);
-  for (let j = 0; j < m; j++) {
-    result[i].push(matrix[m - 1 - j][i]);
+for (let i = 0; i < Math.ceil(n / 2); i++) {
+  result[i] = [];
+  result[n - 1 - i] = [];
+  for (let j = 0; j < Math.ceil(m / 2); j++) {
+    result[i][j] = matrix[m - 1 - j][i];
+    result[i][m - 1 - j] = matrix[j][i];
+
+    result[n - 1 - i][j] = matrix[m - 1 - j][n - 1 - i];
+    result[n - 1 - i][m - 1 - j] = matrix[j][n - 1 - i];
   }
 }
 
